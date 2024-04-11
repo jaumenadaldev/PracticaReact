d948c8b1a670ab560f6ca11207b428a2f2dd2890
JANACA <jaumenadaldev@gmail.com>
2024-04-11 12:06:32 +0200

    añadir reserva

--- src/Hotels.tsx
+++ src/Hotels.tsx
@@ -1,30 +1,51 @@
-import { useSelector } from "react-redux";
-import { HabitacionSingle } from "../src/store/Habitaciones";
-import { RootState } from "../src/store/store";
+import { useSelector, useDispatch } from "react-redux";
+import { HabitacionSingle, setHabitaciones, selectHabitaciones, cancelarReserva } from "../src/store/Habitaciones";
+import { useState } from "react";
 
 
 export default function Hoteles() {
-  const { habitaciones } = useSelector((state: RootState) => state.Habitaciones);
+  const dispatch = useDispatch();
+  const { habitaciones } = useSelector(selectHabitaciones);
+  const [checkedRooms, setCheckedRooms] = useState<number[]>([]);
 
-  function Submit2(e: any) {
+  function handleCheckboxChange(e: React.ChangeEvent<HTMLInputElement>, num: number) {
+    if (e.target.checked) {
+      setCheckedRooms(prev => [...prev, num]);
+    } else {
+      setCheckedRooms(prev => prev.filter(n => n !== num));
+    }
+  }
+  function handleCancelClick() {
+  checkedRooms.forEach(num => {
+    dispatch(cancelarReserva(num));
+  });
+  setCheckedRooms([]); // Limpiar las habitaciones seleccionadas
+}
+
+  function Submit2(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
-    console.log(habitaciones.map((habitacion: HabitacionSingle) => habitacion.state));
+    checkedRooms.forEach(num => {
+      const habitacion = habitaciones.find(h => h.num === num);
+      if (habitacion && habitacion.state === "Libre") {
+        dispatch(setHabitaciones({ ...habitacion, state: "Ocupada" }));
+      }
+    });
   }
 
-  return (
+ return (
     <form style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }} onSubmit={Submit2}>
       <div style={{ display: 'grid', justifyContent: 'center', alignItems: 'center', border: '3px solid black', borderRadius: '10px', width: '300px' }}>
         {habitaciones.map((habitacion: HabitacionSingle) => (
           <div key={habitacion.num} style={{ marginBottom: '10px' }}>
             <label style={{ color: 'green' }}>
-              <input type="checkbox" />
+              <input type="checkbox" onChange={e => handleCheckboxChange(e, habitacion.num)} />
               Habitación {habitacion.num} | {habitacion.state}
             </label>
           </div>
         ))}
         <div style={{ display: 'flex', justifyContent: 'space-between', width: '100px', margin: '20px auto' }}>
-          <input style={{ marginRight: '10px', marginLeft: '-30px' }} type="submit" value="Submit" />
-          <button style={{ marginLeft: '10px' }}>Cancelar reserva</button>
+          <input style={{ marginRight: '10px', marginLeft: '-30px' }} type="submit" value="Reservar" />
+          <button style={{ marginLeft: '10px' }} onClick={handleCancelClick}>Cancelar reserva</button>
         </div>
       </div>
     </form>
--- src/store/Habitaciones.ts
+++ src/store/Habitaciones.ts
@@ -1,50 +1,43 @@
-import { createSlice } from "@reduxjs/toolkit";
-
-interface ChangeRoom{
-    payload: {
-        num: number,
-        state: "Libre" | "Ocupada"
-    }
-}
-
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 export interface HabitacionSingle{
     num: number,
     state: "Libre" | "Ocupada"
 }
 
-interface HabitacionesType {
-    habitaciones: HabitacionSingle[]
+interface HabitacionesState {
+  habitaciones: HabitacionSingle[];
 }
 
-const initialHabitaciones: HabitacionSingle[] = [
-    {num: 1, state: "Libre"},
-    {num: 2, state: "Libre"},
-    {num: 3, state: "Libre"},
-    {num: 4, state: "Libre"},
-    {num: 5, state: "Libre"},
-    {num: 6, state: "Libre"},
-    {num: 7, state: "Libre"},
-    {num: 8, state: "Libre"},
-    {num: 9, state: "Libre"},
-    {num: 10, state: "Libre"}
-]
+const initialState: HabitacionesState = {
+  habitaciones: Array.from({ length: 10 }, (_, i) => ({
+    num: i + 1,
+    state: "Libre" as const,
+  })),
+};
 
-const initialState: HabitacionesType= {
-    habitaciones: initialHabitaciones
-}
 const HabitacionesSlice = createSlice({
     name: 'Habitaciones',
     initialState:initialState,
     reducers: {
-        setHabitaciones: (state, action: ChangeRoom) => {
-            const habitacionToUpdate = state.habitaciones.find(habitacion => habitacion.num === action.payload.num);
-            if (habitacionToUpdate != undefined) {
-                let copyArray = [...state.habitaciones];
-                
+        setHabitaciones: (state, action: PayloadAction<HabitacionSingle>) => {
+            const habitacionToUpdate = state.habitaciones.find(
+                habitacion => habitacion.num === action.payload.num
+            );
+            if (habitacionToUpdate) {
+                habitacionToUpdate.state = action.payload.state; 
             }
-        }
-    }
-})
+        },
+        cancelarReserva: (state, action: PayloadAction<number>) => {
+      const habitacionToUpdate = state.habitaciones.find(
+        (habitacion) => habitacion.num === action.payload
+      );
+      if (habitacionToUpdate) {
+        habitacionToUpdate.state = "Libre";
+      }
+    },
+    },
+});
 
-export const { setHabitaciones } = HabitacionesSlice.actions
+export const { setHabitaciones, cancelarReserva } = HabitacionesSlice.actions;
+export const selectHabitaciones = (state: { Habitaciones: HabitacionesState }) => state.Habitaciones;
 export default HabitacionesSlice.reducer;
